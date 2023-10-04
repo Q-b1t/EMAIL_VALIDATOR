7872ba5ba4daf6fa658dd9665665f5031227d9ec
Q-b1t <ldhq_rhiq@protonmail.com>
2023-10-03 22:07:12 -0600

    now it supponts two apis + improved cli commands and more input formats

--- auxiliary_modules/email_validation_utils.py
+++ auxiliary_modules/email_validation_utils.py
@@ -2,24 +2,107 @@ import requests
 import json
 from tqdm.auto import tqdm
 import pandas as pd
+import os
+from termcolor import colored
+import configparser
+
+
+def get_api_key(config_file,api,verbose):
+    # parse the configuration file
+    validate_config_file(config_file=config_file)
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    assert api == "abstract" or api == "debounce", colored("The specified api {api} is not valid. Please use \"abstract\" or \"debounce\"","red")
+    assert config['API_KEYS'][api] != "" ,colored(f"[-] The configuration file does not contain a {api} API key of the format is not the appropiate one.","red")
+    return config['API_KEYS'][api]
+
+def validate_config_file(config_file):
+    assert  os.path.isfile(config_file),colored(f"[-] Configuration file {config_file} does not seem to exist. Please check for any errors.","red")
+
+def validate_output_file(output_file):
+    # validate if outfile file contains no extentions and if the save path exist
+    assert "xlsx" not in output_file or  "csv" not in output_file, colored(f"[-] The output file should only be limited to the name. Please ommit the extention.","red")
+    if output_file.find("/") != -1 or output_file.find("\\") != -1:
+        path = os.path.dirname(output_file)
+        assert os.path.exists(path=path), colored(f"The path {path} does not seem to exist.","red")
+
+def validate_name(filename,verbose):
+    # validate if the input file exist and if it contains an allowed extention
+    assert  os.path.isfile(filename),colored(f"[-] Specified file {filename} does not seem to exist. Please check for any errors.","red")
+    extention = filename.split(".")[-1]
+    assert  extention != "csv" or extention != "txt" or extention != "xlsx" ,colored(f"[-] Specified file {filename} is not in an allowed format (xlsx,csv,txt).","red")
+    if verbose:
+        print(colored(f"[+] File {filename} found.","green"))
 
 def debounce_api_request(mail,api_key):
     url = f"https://api.debounce.io/v1/?api={api_key}&email={mail}"
     response = requests.get(url=url)
     return response
 
-def get_verification_responses(mail_list,api_key,verbose):
+def abstract_api_request(mail,api_key):
+    url = f"https://emailvalidation.abstractapi.com/v1/?api_key={api_key}&email={mail}"
+    response = requests.get(url=url)
+    return response
+
+def read_text_file(filename):
+    with open(filename,"r",encoding="utf-8") as f:
+        lines = f.readlines()
+    f.close()
+    return [line.strip() for line in lines]
+
+def get_mail_df(filename):
+    mails = read_text_file(filename=filename)
+    mails_table = pd.DataFrame({"email":mails})
+    return mails_table 
+
+def get_emails_data_table(input_file,verbose):
+    extention = input_file.split(".")[-1]
+    if verbose:
+        print(colored(f"[*] Reading and normalizing data from file {input_file}","blue"))
+    
+    if extention == "xlsx":
+        input_data = pd.read_excel(input_file)
+    elif extention == "csv":
+        input_data = pd.read_csv(input_file)
+    else: # txt file
+        input_data = get_mail_df(filename=input_file)
+    
+    assert "email" in input_data.columns, colored("[-] The file is not in the correct format. If it is either a csv file or an excel book,  please make sure it has an \"email\" column.","red")
+    input_data["email"] = input_data["email"].apply(lambda x : str(x).lower())
+    return input_data
+
+def get_email_list(input_data,verbose):
+    email_list = list(set(input_data["email"].to_list()))
+    if verbose:
+        print(colored(f"[+] Found {len(email_list)} unique emails.","green"))
+    return email_list
+
+def get_verification_responses(mail_list,api,api_key,verbose):
+    if verbose:
+        print(colored("[*] Validating emails...","blue"))
     responses = list()
     if verbose:
-        for mail in tqdm(mail_list):
-            response = debounce_api_request(mail=mail,api_key=api_key)
-            if response.status_code == 200:
-                responses.append(response)
+        if api == "abstract":
+            for mail in tqdm(mail_list):
+                response = abstract_api_request(mail=mail,api_key=api_key)
+                if response.status_code == 200:
+                    responses.append(response)
+        else:
+            for mail in tqdm(mail_list):
+                response = debounce_api_request(mail=mail,api_key=api_key)
+                if response.status_code == 200:
+                    responses.append(response)      
     else:
-        for mail in mail_list:
-            response = debounce_api_request(mail=mail,api_key=api_key)
-            if response.status_code == 200:
-                responses.append(response) 
+        if api == "abstract":
+            for mail in mail_list:
+                response = abstract_api_request(mail=mail,api_key=api_key)
+                if response.status_code == 200:
+                    responses.append(response) 
+        else:
+            for mail in mail_list:
+                response = debounce_api_request(mail=mail,api_key=api_key)
+                if response.status_code == 200:
+                    responses.append(response) 
     return responses
 
 def process_debounce_sample(json_dump,columns_delete = ["reason","send_transactional"]):
@@ -28,7 +111,21 @@ def process_debounce_sample(json_dump,columns_delete = ["reason","send_transacti
     processed.drop(columns_delete,inplace=True)
     return processed
 
-def get_email_verification_table(json_samples,codes = {
+def process_abstract_sample(json_dump):
+    keys = ["is_valid_format","is_free_email","is_disposable_email","is_role_email","is_catchall_email","is_mx_found","is_smtp_valid"]        
+
+    json_dict = json.loads(json_dump)
+    for key in keys:
+        json_dict[key] = json_dict[key]["text"]
+    return pd.Series(json_dict)
+
+def get_email_verification_table_absract(json_samples):
+    processed_samples = [process_abstract_sample(json_sample) for json_sample in json_samples]
+    processed_table = pd.DataFrame(processed_samples)
+    return processed_table
+
+
+def get_email_verification_table(responses,api,codes = {
     1 : "Not an email",
     2: "Spam-trap by ESPs",
     3: "A temporary, disposable address",
@@ -38,7 +135,29 @@ def get_email_verification_table(json_samples,codes = {
     7: "The server cannot be reached",
     8: "See roles column"
     }):
-    processed_samples = [process_debounce_sample(json_sample) for json_sample in json_samples]
+    json_dumps = [response.text for response in responses]
+    processed_samples = [process_abstract_sample(json_sample) for json_sample in json_dumps] if api == "abstract" else [process_debounce_sample(json_sample) for json_sample in json_dumps]
     processed_table = pd.DataFrame(processed_samples)
-    processed_table["validation_additional_information"] = processed_table["code"].apply(lambda x : codes[int(x)])
+    if "code" in processed_table.columns:
+        processed_table["validation_additional_information"] = processed_table["code"].apply(lambda x : codes[int(x)])
     return processed_table
+
+def merge_table(emails_table,validation_table):
+    merged_table = emails_table.merge(validation_table,how="outer",on="email")
+    return merged_table
+
+def save_table(parsed_table,save_path,output_format,verbose):
+    """
+    Inputs: 
+        - parsed_table: A processed pandas dataframe.
+        - save_path: The to which the table will be saved as an excel book.
+    """
+    if output_format == "csv":
+        filename = save_path + "." + "csv"
+        parsed_table.to_csv(filename,index = False)
+    else:
+        filename = save_path + "." + "xlsx"
+        parsed_table.to_excel(filename, index=False)
+         
+    if verbose:
+        print(colored(f"[+] Exporting the results to {save_path}.","green"))
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import configparser
 from argparse import ArgumentParser,Namespace
 import pandas as pd
 import sys
@@ -11,44 +10,46 @@ if __name__ == '__main__':
     # create the cli arguments
     parser = ArgumentParser()
     parser.add_argument("-c","--config_file",help="Configuration file contianing the API keys (default: \"conf.cfg\")",type=str,default="conf.cfg",nargs="?")
-    parser.add_argument("-i","--input_file",help="Input file containing (excel book) containing the emails to check (default: \"merged_data.xlsx\")",type=str,default="./inputs/merged_data.xlsx",nargs="?")
-    parser.add_argument("-o","--output_file",help="Name of the output file the hostnames will be exported to (default: \"email_verified.xlsx\")",type=str,default="./outputs/email_verified.xlsx",nargs="?")
+    parser.add_argument("-i","--input_file",help="Input file containing (excel book) containing the emails to check (default: \"merged_data.xlsx\")",type=str,default="merged_data.xlsx",nargs="?")
+    parser.add_argument("-o","--output_file",help="Name of the output file the hostnames will be exported to (default: \"email_verified\")",type=str,default="email_verified",nargs="?")
     parser.add_argument("-v","--verbose",help="Whether to output into the console information about the script's progress or not",type=bool,default=False,nargs="?")
+    parser.add_argument("-f","--output_format",help="It can be either \"excel\" or \"csv\" (default: \"excel\")",type=str,default="excel",nargs="?")
+    parser.add_argument("-a","--api",help="It can be either \"abstract\" or \"debounce\" (default: \"abstract\")",type=str,default="abstract",nargs="?")
+    # fetcth the specified parameters
     args: Namespace = parser.parse_args()
     config_file = args.config_file
     input_file = args.input_file
     output_file = args.output_file
     verbose = args.verbose
+    output_format = args.output_format
+    api = args.api
 
-    # parse the configuration file
-    config = configparser.ConfigParser()
-    config.read(config_file)
-    assert config['API_KEY']['debounce'] != None, colored("[-] The configuration file does not contain an API key of the format is not the appropiate one.","red")
 
-    DEBOUNCE_API_KEY = config['API_KEY']['debounce']
+    # retrieve the api key
+    API_KEY = get_api_key(config_file=config_file,api=api,verbose=verbose)
 
-    # validate read the input file 
-    assert input_file.split(".")[1] != "xlsx" and input_file.split(".")[1] != "csv",colored("[-] Input file must be either an excel book or a csv file.","red")
-    if verbose:
-        print(colored(f"[*] Reading file {input_file}","blue"))
-    input_data = pd.read_excel(input_file)
-    input_data["email"] = input_data["email"].apply(lambda x : str(x).lower())
+    # validate the input file
+    validate_name(filename=input_file,verbose=verbose)
     
-    # validate if the excel book has a valid format 
-    assert "email" in input_data.columns, colored("[-] The file is not in the correct format. Make sure the excel book has \"email\" column.","red")
-    email_list = list(set(input_data["email"].to_list()))
-    if verbose:
-        print(colored(f"[+] Found {len(email_list)} potential emails.","green"))
+    # validate the output file
+    validate_output_file(output_file=output_file)
+
+    # fetch the data from the input file
+    input_data = get_emails_data_table(input_file=input_file,verbose=verbose)
     
+    # get emails list
+    email_list = get_email_list(input_data=input_data,verbose=verbose)
+
     # make the api requests and parse the responses into a table
-    if verbose:
-        print(colored("[*] Validating emails...","blue"))
-    responses = get_verification_responses(mail_list=email_list,api_key=DEBOUNCE_API_KEY,verbose=verbose)
-    json_dumps = [response.text for response in responses]
-    email_verification_tables = get_email_verification_table(json_samples=json_dumps)
-    # merge the responses with the original data and export to the output file
-    assert output_file.split(".")[1] != "xlsx" or output_file.split(".")[1] != "csv",colored("[-] Output file must be either an excel book or a csv file.","red")
-    if verbose:
-        print(colored(f"[+] Exporting the results to {output_file}.","green"))
-    export_data = input_data.merge(email_verification_tables,how="outer",on="email")
-    export_data.to_excel(output_file)
+    responses = get_verification_responses(mail_list=email_list,api=api,api_key=API_KEY,verbose=verbose)
+
+    # parse the responses
+    email_verification_table = get_email_verification_table(responses=responses,api=api)
+
+    # merge the original input data with the verification table
+    merged_table = merge_table(emails_table=input_data,validation_table=email_verification_table)
+
+    # save the table
+    save_table(parsed_table=merged_table,save_path=output_file,output_format=output_format,verbose=verbose)
+
+        
